3fadbb1b45c113c54f609332ef8ba1e29a558b62
tm-ahad <tm.ahad.07@gmail.com>
2022-09-09 21:59:31 +0600

    guessing game completed.

--- r-01-getting-started/src/main.rs
+++ r-01-getting-started/src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Hello, world!ddd");
+    println!("Hello, world!");
 }
--- r-02-guessing-game/src/main.rs
+++ r-02-guessing-game/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::stdin;
+use colored::Colorize;
+
+pub fn welcome() {
+    println!("{}{}", "Welcome to our ".yellow(), "guessing game".bold().italic().blue())
+}
+
+pub fn get_input_and_print() {
+    let mut input: String = String::new();
+
+    match stdin().read_line(&mut input) {
+        Ok(_) => {
+            println!("You guessed: {}", input.purple().italic());
+
+            get_input_and_print();
+        },
+        Err(e) => panic!("{}", e)
+    }
+}
+
+pub fn main() {
+    welcome();
+    get_input_and_print();
 }
