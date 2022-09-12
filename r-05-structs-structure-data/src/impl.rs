432145ef31aeab59f69f75c10c8cc74b942b9239
tm-ahad <tm.ahad.07@gmail.com>
2022-09-12 22:31:17 +0600

    impl completed.

---
+++ r-05-structs-structure-data/src/impl.rs
@@ -0,0 +1,36 @@
+
+#[derive(Debug)]
+pub enum Color {
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Black,
+    White,
+    Brown,
+    Chocolate,
+    Pink,
+    Violet
+}
+
+#[derive(Debug)]
+pub struct Square {
+    pub height: i128,
+    pub name: String,
+    pub color: Color
+}
+
+impl Square {
+
+    pub fn create(height: i128, name: String, color: Color) -> Square {
+        Square {
+            height,
+            name,
+            color
+        }
+    }
+
+    pub fn get_area(&self) -> i128 {
+        return self.height * self.height;
+    }
+}
--- r-05-structs-structure-data/src/main.rs
+++ r-05-structs-structure-data/src/main.rs
@@ -1,31 +1,45 @@
 
-use crate::r#struct::Person;
+//struct
 
-mod r#struct;
+// use crate::r#struct::Person;
+//
+// mod r#struct;
+// mod r#impl;
+//
+// fn main() {
+//     //creating a person
+//     let person = Person {
+//         name: String::from("Tah-mid"),
+//         age: 9,
+//         email: String::from("tm.ahad.bd@gmail.com"),
+//         phone: 123456789987654
+//     };
+//
+//     //creating a constructor
+//
+//     let person2 = create_person("Fah-mid", 5, "fm.ahad.bd@gmail.com", 45678954458);
+//
+//     println!("person1 = {:?},\nperson2 = {:?}", person, person2);
+// }
+//
+// pub fn create_person(name: &str, age: i8, email: &str, phone: i128) -> Person {
+//     let str_name = String::from(name);
+//     let str_email = String::from(email);
+//     Person {
+//         name: str_name,
+//         age,
+//         email: str_email,
+//         phone
+//     }
+// }
+
+mod r#impl;
+pub use r#impl::{Square, Color};
 
 fn main() {
-    //creating a person
-    let person = Person {
-        name: String::from("Tah-mid"),
-        age: 9,
-        email: String::from("tm.ahad.bd@gmail.com"),
-        phone: 123456789987654
-    };
+    let sqr1: Square = Square::create(5, String::from("Tah-mid"), Color::Red);
 
-    //creating a constructor
+    println!("{:?}", sqr1);
 
-    let person2 = create_person("Fah-mid", 5, "fm.ahad.bd@gmail.com", 45678954458);
-
-    println!("person1 = {:?},\nperson2 = {:?}", person, person2);
-}
-
-pub fn create_person(name: &str, age: i8, email: &str, phone: i128) -> Person {
-    let str_name = String::from(name);
-    let str_email = String::from(email);
-    Person {
-        name: str_name,
-        age,
-        email: str_email,
-        phone
-    }
+    println!("area={}", sqr1.get_area());
 }
