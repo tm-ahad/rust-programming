9fcdd9b54b032fbb4a59b14c97d51fa5168f3c88
tm-ahad <tm.ahad.07@gmail.com>
2022-09-12 15:12:49 +0600

    struct complete.

--- r-05-structs-structure-data/src/main.rs
+++ r-05-structs-structure-data/src/main.rs
@@ -1,3 +1,31 @@
+
+use crate::r#struct::Person;
+
+mod r#struct;
+
 fn main() {
-    println!("Hello, world!");
+    //creating a person
+    let person = Person {
+        name: String::from("Tah-mid"),
+        age: 9,
+        email: String::from("tm.ahad.bd@gmail.com"),
+        phone: 123456789987654
+    };
+
+    //creating a constructor
+
+    let person2 = create_person("Fah-mid", 5, "fm.ahad.bd@gmail.com", 45678954458);
+
+    println!("person1 = {:?},\nperson2 = {:?}", person, person2);
+}
+
+pub fn create_person(name: &str, age: i8, email: &str, phone: i128) -> Person {
+    let str_name = String::from(name);
+    let str_email = String::from(email);
+    Person {
+        name: str_name,
+        age,
+        email: str_email,
+        phone
+    }
 }
---
+++ r-05-structs-structure-data/src/struct.rs
@@ -0,0 +1,7 @@
+#[derive(Debug)]
+pub struct Person {
+    pub name: String,
+    pub age: i8,
+    pub email: String,
+    pub phone: i128
+}
