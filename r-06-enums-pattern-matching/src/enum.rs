6f3c5e1b887345d8fdeb05ea6d21569d2113200c
tm-ahad <tm.ahad.07@gmail.com>
2022-09-14 12:59:07 +0600

    EPM going on.

---
+++ r-06-enums-pattern-matching/src/enum.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub enum MarvelSuperHeroes {
+    SpiderMan(Spidey),
+    Venom(VenomHost),
+    Hulk,
+    DeadPool
+}
+#[derive(Debug)]
+pub enum DCSuperHero {
+    BlackAdam,
+    SuperMan,
+    Shazam,
+    Flash
+}
+#[derive(Debug)]
+pub enum Spidey {
+    Tobey,
+    Andrew,
+    Tom,
+    MilesMorales,
+    Flash
+}
+
+#[derive(Debug)]
+pub enum VenomHost {
+    Eddie,
+    Tobey,
+    Deadpool
+}
--- r-06-enums-pattern-matching/src/main.rs
+++ r-06-enums-pattern-matching/src/main.rs
@@ -1,3 +1,25 @@
+use crate::r#enum::{MarvelSuperHeroes, Spidey, VenomHost};
+
+mod r#enum;
+
 fn main() {
-    println!("Hello, world!");
+    let spider_man = MarvelSuperHeroes::SpiderMan(Spidey::Tobey);
+    let venom = MarvelSuperHeroes::Venom(VenomHost::Eddie);
+
+    //matching enum
+
+    match spider_man {
+        MarvelSuperHeroes::SpiderMan(c) => println!("{:?} is spider-man", c),
+        _ => {}
+    }
+
+    //You can watch the Venom movie
+
+    match venom {
+        MarvelSuperHeroes::Venom(host) => println!("{:?} is venom", host),
+        _ => {}
+    }
+
+    //which universe's superhero do you like DC or Marvel
+    //I like DC 👍👍
 }
