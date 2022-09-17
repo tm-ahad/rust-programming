e2d838d76a5173d82649552f2a81183c6ab6e245
tm-ahad <tm.ahad.07@gmail.com>
2022-09-17 20:09:05 +0600

    Vector completed.

--- r-08-common-collection/src/main.rs
+++ r-08-common-collection/src/main.rs
@@ -1,6 +1,7 @@
 
 
 fn main() {
+    //String
     println!("{}", String::from("السلام عليكم"));
     println!("{}", String::from("Dobrý den"));
     println!("{}", String::from("Hello"));
@@ -38,4 +39,59 @@ fn main() {
         }
     }
 
+    //vector
+    println!("=====================Vector======================");
+
+    let v = vec![1, 2, 3];
+
+    let mut v2 = Vec::new();
+
+    v2.push(5);
+    v2.push(6);
+    v2.push(7);
+    v2.push(8);
+
+    println!("{:?}\n{:?}\n", v, v2);
+
+    let v3 = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v3[2];
+
+    //If there are no value at the index it's panic
+    println!("The third element is {}", third);
+
+    //It's a Option
+    let third: Option<&i32> = v.get(2);
+    match third
+    {
+        Some(third) => println!("The third element is {}.\n", third),
+        None => println!("There is no third element.\n"),
+    }
+
+    let mut v3 = vec![100, 32, 57];
+
+    v3.clear();
+
+    println!("v3 = {:?}", v3);
+
+    v3 = vec![100, 32, 58];
+    v3.reverse();
+    v3.remove(1);
+    v3.push(78);
+    v3.push(87);
+    v3.repeat(3);
+
+    println!("v3 = {:?}\n", v3);
+
+    for i in &mut v3 {
+        *i += 50;
+
+        println!("i - {}", i);
+    }
+
+    //There are many Methods available in vector-
+    //reverse
+    //remove
+    //replace
+    //repeat
 }
