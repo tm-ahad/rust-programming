6c24e1abf2fbd9a94ef08fd85cac8332ba4d35ea
tm-ahad <tm.ahad.07@gmail.com>
2022-09-20 20:35:19 +0600

    chapter 10 completed.

--- r-10-generic-traits-lifetimes/src/main.rs
+++ r-10-generic-traits-lifetimes/src/main.rs
@@ -1,3 +1,44 @@
 fn main() {
-    println!("Hello, world!");
+    // let r;
+    //
+    // {
+    //     let x = String::from("Fah-mid ");
+    //     r = &x;
+    // }
+
+    // println!("r: {}", r);
+
+    let str1: &str = "Tah-mid";
+    let str2: &str = "Fah-mid";
+
+    let res = shortest(str1, str2);
+
+    println!("{}", res);
+
+    //r -> &x -> x
+    //but x is destroyed.
+}
+
+//error
+// pub fn shortest(str1: &str, str2: &str) -> &str
+// {
+//     //because of the str1 or str2 is will finish after execute the function
+//
+//     if str1.len() < str2.len()
+//     {
+//         return str1;
+//     }
+//     str2
+// }
+
+
+pub fn shortest<'a>(str1: &'a str, str2: &'a str) -> &'a str
+{
+    //because of the str1 or str2 is will finish after execute the function
+
+    if str1.len() < str2.len()
+    {
+        return str1;
+    }
+    str2
 }
