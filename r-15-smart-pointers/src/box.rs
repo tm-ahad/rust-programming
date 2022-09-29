c53850cc0235e44db6a8a572394570460d4902b3
tm-ahad <tm.ahad.07@gmail.com>
2022-09-29 21:54:15 +0600

    box complete.

---
+++ r-15-smart-pointers/src/box.rs
@@ -0,0 +1,18 @@
+use crate::r#box::List::Cons;
+
+#[derive(Debug)]
+pub enum List
+{
+    Cons(i32, Box<List>),
+    Nil
+}
+
+pub fn understand_box()
+{
+    //box store a value in heap
+    let b = Box::new(8);
+    let list1 = Cons(8, Box::new(List::Nil));
+
+    println!("{}", b);
+    println!("{:?}", list1);
+}
--- r-15-smart-pointers/src/main.rs
+++ r-15-smart-pointers/src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+mod r#box;
+pub(crate) use r#box::understand_box;
+
+fn main()
+{
+    understand_box()
+}
