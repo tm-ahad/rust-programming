69c21e9bff7d6633f526326b03fc7ccb1f78d084
tm-ahad <tm.ahad.07@gmail.com>
2022-10-09 11:46:19 +0600

    concurrency going on.

--- r-16-fearless-concurrency/src/main.rs
+++ r-16-fearless-concurrency/src/main.rs
@@ -1,3 +1,12 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    thread::spawn(|| {
+        println!("stuff 1 from spawned thread");
+        println!("stuff 2 from spawned thread");
+    });
+
+    println!("stuff 1 from main thread");
+    println!("stuff 2 from main thread");
 }
