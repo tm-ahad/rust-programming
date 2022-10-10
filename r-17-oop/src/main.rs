6a16b31a4c16f957e136fb01940c782603e314a2
tm-ahad <tm.ahad.07@gmail.com>
2022-10-10 17:04:08 +0600

    Trait completed.

--- r-17-oop/src/main.rs
+++ r-17-oop/src/main.rs
@@ -1,6 +1,20 @@
 mod lib;
+mod r#trait;
 
 use lib::AveragedCollection;
+use crate::r#trait::{Draw, Button, Screen};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("{}", "Drawing Select box...")
+    }
+}
 
 fn main() {
     let mut a = AveragedCollection::new(vec![1, 2, 5], (8 / 3) as f64);
@@ -9,5 +23,28 @@ fn main() {
 
     a.add(6);
 
-    println!("{:?}", a.remove().unwrap()) //last element;
+    println!("{:?}", a.remove().unwrap()); //last element;
+
+    //trait
+
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
 }
