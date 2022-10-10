877d85721d7473116005026329231c8e29513ae9
tm-ahad <tm.ahad.07@gmail.com>
2022-10-10 16:51:43 +0600

    chapter 17 going on.

--- r-17-oop/src/main.rs
+++ r-17-oop/src/main.rs
@@ -1,3 +1,13 @@
+mod lib;
+
+use lib::AveragedCollection;
+
 fn main() {
-    println!("Hello, world!");
+    let mut a = AveragedCollection::new(vec![1, 2, 5], (8 / 3) as f64);
+
+    println!("{:?}", a.average());
+
+    a.add(6);
+
+    println!("{:?}", a.remove().unwrap()) //last element;
 }
